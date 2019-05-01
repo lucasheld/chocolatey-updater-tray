8d64e44f4d0adb2c47553f8802170c38ab59592c
Witek094 <Witek094@gmail.com>
2019-05-01 13:32:43 +0200

    Update choco-updater.ps1

--- choco-updater.ps1
+++ choco-updater.ps1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName System.Windows.Forms 
 
 function getListOfUpdates(){
-    [System.Collections.ArrayList]$updateList = choco outdated -r | % { $_.split("|")[0] }} 
+    [System.Collections.ArrayList]$updateList = choco outdated -r | % { $_.split("|")[0] }
     return $updateList
 }
 
